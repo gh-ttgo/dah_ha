8059b1a364a5a322589dc1aed18cf99a9e5c18d9
gh-ttgo <61864794+gh-ttgo@users.noreply.github.com>
2025-09-29 10:42:57 +0200

    Create const.py

---
+++ const.py
@@ -0,0 +1,4 @@
+DOMAIN = "dahsolar"
+INDEX_PAGE = "https://cloud.dahsolar.com/index"
+LOGIN_ENDPOINT = "https://interface.dhhome-e.com/dh/auth/login"
+API_BASE = "https://interface.dhhome-e.com/dh/"
